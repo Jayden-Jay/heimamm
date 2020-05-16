ddc00d6b156d78d3cf66f34d366f801c759e5061
Jayden-Jay <727154523@qq.com>
2020-05-16 23:07:55 +0800

    2.初始化数据-新建文件夹以及文件，路由的配置

---
+++ src/router/index.js
@@ -0,0 +1,36 @@
+// 导入Vue
+import Vue from 'vue'
+
+// 导入路由
+import VueRouter from 'vue-router'
+
+//导入组件
+import Login from '@/views/login'
+import Login from '@/views/layout'
+
+// 注册路由
+Vue.use(VueRouter);
+
+// 创建路由规则
+let routes = [
+    {
+        path:'/login',
+        component:Login
+    },
+    {
+        path:'/layout',
+        component:Layout
+    },
+    {
+        path:'*',
+        redirect: '/login'
+    }
+]
+
+// 创建路由对象
+let router = new VueRouter({
+    routes
+});
+
+// 暴露出去
+export default router;
